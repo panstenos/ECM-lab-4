59352ab4244c62fbeff4f712454c97acbf18576b
panstenos <112823396+panstenos@users.noreply.github.com>
2022-11-08 12:51:24 +0000

    Restore

--- main.c
+++ main.c
@@ -14,15 +14,20 @@
 
 void main(void) {
     LCD_Init(); //initialise LCD
-    ADC_init(); //initialise ADC
+    ADC_init();
     char *buf; 
     __delay_ms(200);
     while (1) {
         
         LCD_clear(); //clear screen function
+        //LCD_setline(1);
+        // LCD_sendbyte((0x0E,0x1F,0x04,0x0C,0x0F,0x04,0x08,0x0A),1); //H
+        //LCD_sendbyte(0xD2,1); //E
+        //0x0E,0x1F,0x04,0x0C,0x0F,0x04,0x08,0x0A
         ADC2String(*buf, ADC_getval()); //convert integer into string
-        LCD_sendstring(*buf,"max = 3.3V"); // print ldr value /255
+        LCD_sendstring(*buf,"max = 255"); // print ldr value /255
         __delay_ms(100);
         
     }
 }
+
